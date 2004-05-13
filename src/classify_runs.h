455fd7584d5aaea71d734ec29d614d99eac2f583
Mayur Naik <mhnaik@seas.upenn.edu>
2004-05-13 04:10:53 +0000

    *** empty log message ***

--- src/classify_runs.h
+++
@@ -1,9 +0,0 @@
-#ifndef CLASSIFY_RUNS_H
-#define CLASSIFY_RUNS_H
-
-extern int num_runs, num_sruns, num_fruns;
-extern bool *is_srun, *is_frun;
-
-extern void classify_runs();
-
-#endif
--- src/scaffold.h
+++
@@ -1,6 +0,0 @@
-#ifndef SCAFFOLD_H
-#define SCAFFOLD_H
-
-void scaffold(void (*process_site)(FILE*, int, int, int));
-
-#endif
