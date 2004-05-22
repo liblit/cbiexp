495b776d1b5d552e4731f1143895222598a89f71
Mayur Naik <mhnaik@seas.upenn.edu>
2004-05-22 01:15:53 +0000

    Implemented two optimizations for pruning the list of predicates that survive the (lower bound of increase score > 0) filter. Let lb(p) denote the lower bound of increase score of predicate p rounded to the nearest integer.  Then, the optimizations are: 1. Within a scalar-pairs site or a returns site:    eliminate predicate p1 if it is logically implied by predicate p2    at the same site such that lb(p1) = lb(p2).    This is done by cull_preds_aggressively1(). 2. Across scalar-pairs sites on the same line in a C source file:    eliminate predicate p1 of the form x < c or x <= c or x > c or x >= c    if it is logically implied by predicate p2 on the same line such that    lb(p1) = lb(p2).    This is done by cull_preds_aggressively2().

--- src/compute_results.cc
+++ src/compute_results.cc
@@ -13,10 +13,13 @@
 
 using namespace std;
 
+enum { LT, GEQ, EQ, NEQ, GT, LEQ };
+
 struct site_summary_t {
     int S[6], F[6];
     int os, of;
     bool retain[6];
+    int lb[6]; 
     site_summary_t()
     {
 	os = of = 0;
@@ -24,6 +27,7 @@ struct site_summary_t {
 	    S[i] = 0;
 	    F[i] = 0;
             retain[i] = false;
+            lb[i] = -1;
 	}
     }
 };
@@ -62,6 +66,123 @@ int num_r_preds = 0;
 int num_b_preds = 0;
 
 FILE* preds_txt_fp = NULL;
+
+/****************************************************************************
+ * Procedures for printing (1) result summary and (2) retained predicates
+ ***************************************************************************/
+
+const char* scalar_op[6] = { "<", ">=", "==", "!=", ">", "<=" };
+const char* branch_op[2] = { "is FALSE", "is TRUE" };
+
+void print_result_summary()
+{
+    time_t t;
+    time(&t);
+
+    FILE* fp = fopen(result_summary_htm_file, "w");
+    assert(fp);
+
+    fputs("<table>\n<tr>\n"
+	  "<td align=middle><font size=\"+2\">Cooperative Bug Isolation Report</font></td>\n"
+	  "<td rowSpan=2 align=right>"
+	  "<a  href=\"http://www.cs.berkeley.edu/~liblit/sampler/\">"
+          "<img src=\"http://www.cs.berkeley.edu/~liblit/sampler/logo.png\" style=\"border-style: none\"></a></td>\n</tr>\n"
+	  "</table>\n<br>\n",
+	  fp);
+    fprintf(fp, "Experiment name: %s\n<p>\n", experiment_name);
+    fprintf(fp, "Generated on %s<p>\n", ctime(&t));
+    fprintf(fp, "# runs: %d [successful: %d failing: %d discarded: %d]\n<p>\n",
+	num_runs, num_sruns, num_fruns, num_runs - (num_sruns + num_fruns));
+    fprintf(fp, "# predicates instrumented: %d [branch: %d return: %d scalar: %d]\n<p>\n",
+        NumBPreds + NumRPreds + NumSPreds, NumBPreds, NumRPreds, NumSPreds);
+    fprintf(fp, "# predicates retained:     %d [branch: %d return: %d scalar: %d]\n<p>\n",
+	num_b_preds + num_r_preds + num_s_preds, num_b_preds, num_r_preds, num_s_preds);
+    fprintf(fp, "Confidence: %d%%\n<p>\n", confidence);
+
+    fclose(fp);
+}
+
+inline void print_pred_info(FILE* fp, int u, int c, int p)
+{
+    int s  = site_summary[u][c].S[p];
+    int f  = site_summary[u][c].F[p];
+    int os = site_summary[u][c].os;
+    int of = site_summary[u][c].of;
+
+    float fs = ((float)  f) / ( s +  f);
+    float co = ((float) of) / (os + of);
+    float in = fs - co;
+    float lb = in - conf * sqrt(((fs * (1 - fs)) / (f + s)) + ((co * (1 - co))/(of + os)));
+
+    fprintf(fp, "%c %d %d %d %.2f %.2f %.2f %.2f %d %d %d %d ",
+	units[u].s[0], u, c, p, lb, in, fs, co, s, f, os, of);
+}
+
+inline void print_site_info(FILE* fp, int i)
+{
+    fprintf(fp, "</td><td>%s</td><td>%s:%d\n", sites[i].fun, sites[i].file, sites[i].line);
+}
+
+inline void print_b_pred_name(FILE* fp, int i, const char* op)
+{
+    fprintf(fp, "%s %s", sites[i].args[0], op);
+}
+
+inline void print_s_pred_name(FILE* fp, int i, const char* op)
+{
+    fprintf(fp, "%s %s %s", sites[i].args[0], op, sites[i].args[1]);
+}
+
+inline void print_r_pred_name(FILE* fp, int i, const char* op)
+{
+    fprintf(fp, "%s %s 0", sites[i].args[0], op);
+}
+
+void print_retained_preds()
+{
+    int u, c, p, i = 0;
+
+    FILE* fp = fopen(preds_txt_file, "w");
+    assert(fp);
+
+    for (u = 0; u < NumUnits; u++) {
+	for (c = 0; c < units[u].c; c++, i++) {
+            switch (units[u].s[0]) {
+            case 'S':
+	        for (p = 0; p < 6; p++)
+	            if (site_summary[u][c].retain[p]) {
+		        num_s_preds++;
+		        print_pred_info(fp, u, c, p);
+		        print_s_pred_name(fp, i, scalar_op[p]);
+                        print_site_info(fp, i);
+	            }
+	        break;
+            case 'R':
+	        for (p = 0; p < 6; p++)
+	            if (site_summary[u][c].retain[p]) {
+		        num_r_preds++;
+	                print_pred_info(fp, u, c, p);
+		        print_r_pred_name(fp, i, scalar_op[p]);
+                        print_site_info(fp, i);
+                    }
+	        break;
+            case 'B':
+	        for (p = 0; p < 2; p++)
+	            if (site_summary[u][c].retain[p]) {
+	                num_b_preds++;
+	                print_pred_info(fp, u, c, p);
+	                print_b_pred_name(fp, i, branch_op[p]);
+                        print_site_info(fp, i);
+                    }
+	        break;
+            default:
+                assert(0);
+            }
+        }
+    }
+
+    fclose(fp);
+}
 
 /****************************************************************************
  * Procedures for deciding whether to retain/discard
@@ -133,8 +254,10 @@ inline void cull(int u, int c, int p)
     float in = fs - co;
     float lb = in - conf * sqrt(((fs * (1 - fs)) / (f + s)) + ((co * (1 - co))/(of + os)));
 
-    if (lb > 0 && s + f >= 10) 
+    if (lb > 0 && s + f >= 10) {
         site_summary[u][c].retain[p] = true;
+        site_summary[u][c].lb[p] = (int) rint(lb * 100);  // cache lb for aggressive culling
+    }
 }
 
 void cull_preds()
@@ -163,9 +286,13 @@ void cull_preds()
     }
 }
 
-void cull_preds_aggressively()
+inline bool have_equal_increase(int u1, int c1, int p1, int u2, int c2, int p2)
 {
-    enum { LT, GEQ, EQ, NEQ, GT, LEQ };
+    return site_summary[u1][c1].lb[p1] == site_summary[u2][c2].lb[p2];
+}
+
+void cull_preds_aggressively1()
+{
     int u, c;
 
     for (u = 0; u < NumUnits; u++) {
@@ -179,84 +306,84 @@ void cull_preds_aggressively()
                 if (s.retain[LEQ] &&
                     s.retain[LT ] &&
                     s.retain[EQ ] &&
-                    s.S[LEQ] == s.S[LT] && s.F[LEQ] == s.F[LT] &&
-                    s.S[LEQ] == s.S[EQ] && s.F[LEQ] == s.F[EQ]) {
+                    have_equal_increase(u, c, LT, u, c, LEQ) &&
+                    have_equal_increase(u, c, EQ, u, c, LEQ)) {
                     s.retain[LT] = s.retain[EQ] = false;
                     break;
                 }             
                 if (s.retain[GEQ] &&
                     s.retain[GT ] &&
                     s.retain[EQ ] &&
-                    s.S[GEQ] == s.S[GT] && s.F[GEQ] == s.F[GT] &&
-                    s.S[GEQ] == s.S[EQ] && s.F[GEQ] == s.F[EQ]) {
+                    have_equal_increase(u, c, GT, u, c, GEQ) &&
+                    have_equal_increase(u, c, EQ, u, c, GEQ)) {
                     s.retain[GT] = s.retain[EQ] = false;
                     break;
                 }
                 if (s.retain[NEQ] &&
                     s.retain[LT ] &&
                     s.retain[GT ] &&
-                    s.S[NEQ] == s.S[LT] && s.F[NEQ] == s.F[LT] &&
-                    s.S[NEQ] == s.S[GT] && s.F[NEQ] == s.F[GT]) {
+                    have_equal_increase(u, c, LT, u, c, NEQ) &&
+                    have_equal_increase(u, c, GT, u, c, NEQ)) {
                     s.retain[LT] = s.retain[GT] = false;
                     break;
                 }
                 if (s.retain[LT ] &&
                     s.retain[LEQ] &&
                     s.retain[NEQ] &&
-                    s.S[LT] == s.S[LEQ] && s.F[LT] == s.F[LEQ] &&
-                    s.S[LT] == s.S[NEQ] && s.F[LT] == s.F[NEQ]) {
+                    have_equal_increase(u, c, LEQ, u, c, LT) &&
+                    have_equal_increase(u, c, NEQ, u, c, LT)) {
                     s.retain[LEQ] = s.retain[NEQ] = false;
                     break;
                 }
                 if (s.retain[GT ] &&
                     s.retain[GEQ] &&
                     s.retain[NEQ] &&
-                    s.S[GT] == s.S[GEQ] && s.F[GT] == s.F[GEQ] &&
-                    s.S[GT] == s.S[NEQ] && s.F[GT] == s.F[NEQ]) {
+                    have_equal_increase(u, c, GEQ, u, c, GT) &&
+                    have_equal_increase(u, c, NEQ, u, c, GT)) {
                     s.retain[GEQ] = s.retain[NEQ] = false;
                     break;
                 }
                 if (s.retain[EQ ] &&
                     s.retain[LEQ] &&
                     s.retain[GEQ] &&
-                    s.S[EQ] == s.S[LEQ] && s.F[EQ] == s.F[LEQ] &&
-                    s.S[EQ] == s.S[GEQ] && s.F[EQ] == s.F[GEQ]) {
+                    have_equal_increase(u, c, LEQ, u, c, EQ) &&
+                    have_equal_increase(u, c, GEQ, u, c, EQ)) {
                     s.retain[LEQ] = s.retain[GEQ] = false;
                     break;
                 }
                 if (s.retain[LT ] &&
                     s.retain[LEQ] &&
-                    s.S[LT] == s.S[LEQ] && s.F[LT] == s.F[LEQ]) {
+                    have_equal_increase(u, c, LT, u, c, LEQ)) {
                     s.retain[LEQ] = false;
                     break;
                 }
                 if (s.retain[LT ] &&
                     s.retain[NEQ] &&
-                    s.S[LT] == s.S[NEQ] && s.F[LT] == s.F[NEQ]) {
+                    have_equal_increase(u, c, LT, u, c, NEQ)) {
                     s.retain[NEQ] = false;
                     break;
                 }
                 if (s.retain[GT ] &&
                     s.retain[GEQ] &&
-                    s.S[GT] == s.S[GEQ] && s.F[GT] == s.F[GEQ]) {
+                    have_equal_increase(u, c, GT, u, c, GEQ)) {
                     s.retain[GEQ] = false;
                     break;
                 }
                 if (s.retain[GT ] &&
                     s.retain[NEQ] &&
-                    s.S[GT] == s.S[NEQ] && s.F[GT] == s.F[NEQ]) {
+                    have_equal_increase(u, c, GT, u, c, NEQ)) {
                     s.retain[NEQ] = false;
                     break;
                 }
                 if (s.retain[EQ ] &&
                     s.retain[LEQ] &&
-                    s.S[EQ] == s.S[LEQ] && s.F[EQ] == s.F[LEQ]) {
+                    have_equal_increase(u, c, EQ, u, c, LEQ)) {
                     s.retain[LEQ] = false;
                     break;
                 }
                 if (s.retain[EQ ] &&
                     s.retain[GEQ] &&
-                    s.S[EQ] == s.S[GEQ] && s.F[EQ] == s.F[GEQ]) {
+                    have_equal_increase(u, c, EQ, u, c, GEQ)) {
                     s.retain[GEQ] = false;
                     break;
                 }
@@ -264,132 +391,67 @@ void cull_preds_aggressively()
             }
             case 'B':
                 break;
-            default: assert(0);
-            }
-        }
-    }
-}
-
-/****************************************************************************
- * Procedures for printing (1) result summary and (2) retained predicates
- ***************************************************************************/
-
-void print_result_summary()
-{
-    time_t t;
-    time(&t);
-
-    FILE* fp = fopen(result_summary_htm_file, "w");
-    assert(fp);
-
-    fputs("<table>\n<tr>\n"
-	  "<td align=middle><font size=\"+2\">Cooperative Bug Isolation Report</font></td>\n"
-	  "<td rowSpan=2 align=right>"
-	  "<a  href=\"http://www.cs.berkeley.edu/~liblit/sampler/\">"
-          "<img src=\"http://www.cs.berkeley.edu/~liblit/sampler/logo.png\" style=\"border-style: none\"></a></td>\n</tr>\n"
-	  "</table>\n<br>\n",
-	  fp);
-    fprintf(fp, "Experiment name: %s\n<p>\n", experiment_name);
-    fprintf(fp, "Generated on %s<p>\n", ctime(&t));
-    fprintf(fp, "# runs: %d [successful: %d failing: %d discarded: %d]\n<p>\n",
-	num_runs, num_sruns, num_fruns, num_runs - (num_sruns + num_fruns));
-    fprintf(fp, "# predicates instrumented: %d [branch: %d return: %d scalar: %d]\n<p>\n",
-        NumBPreds + NumRPreds + NumSPreds, NumBPreds, NumRPreds, NumSPreds);
-    fprintf(fp, "# predicates retained:     %d [branch: %d return: %d scalar: %d]\n<p>\n",
-	num_b_preds + num_r_preds + num_s_preds, num_b_preds, num_r_preds, num_s_preds);
-    fprintf(fp, "Confidence: %d%%\n<p>\n", confidence);
-
-    fclose(fp);
-}
-
-inline void print_pred_info(FILE* fp, int u, int c, int p)
-{
-    int s  = site_summary[u][c].S[p];
-    int f  = site_summary[u][c].F[p];
-    int os = site_summary[u][c].os;
-    int of = site_summary[u][c].of;
-
-    float fs = ((float)  f) / ( s +  f);
-    float co = ((float) of) / (os + of);
-    float in = fs - co;
-    float lb = in - conf * sqrt(((fs * (1 - fs)) / (f + s)) + ((co * (1 - co))/(of + os)));
-
-    fprintf(fp, "%c %d %d %d %.2f %.2f %.2f %.2f %d %d %d %d ",
-	units[u].s[0], u, c, p, lb, in, fs, co, s, f, os, of);
-}
-
-inline void print_site_info(FILE* fp, int i)
-{
-    fprintf(fp, "</td><td>%s</td><td>%s:%d\n", sites[i].fun, sites[i].file, sites[i].line);
-}
-
-inline void print_s_pred_name(FILE* fp, int i, char op, bool is_neg)
-{
-    if (is_neg) fputs("!(", fp);
-    fprintf(fp, "%s %c %s", sites[i].args[0], op, sites[i].args[1]);
-    if (is_neg) fputc(')', fp);
-}
-
-inline void print_r_pred_name(FILE* fp, int i, const char* op, bool is_neg)
-{
-    if (is_neg) fputs("!(", fp);
-    fprintf(fp, "%s %s", sites[i].args[0], op);
-    if (is_neg) fputc(')',  fp);
-}
-
-inline void print_b_pred_name(FILE* fp, int i, const char* op)
-{
-    fprintf(fp, "%s %s", sites[i].args[0], op);
-}
-
-void print_retained_preds()
-{
-    const char* branch_op[2] = { "is FALSE", "is TRUE" };
-    const char* return_op[3] = { "< 0", "= 0", "> 0" };
-    const char  scalar_op[3] = { '<', '=', '>' };
-
-    int u, c, p, i = 0;
-
-    FILE* fp = fopen(preds_txt_file, "w");
-    assert(fp);
-
-    for (u = 0; u < NumUnits; u++) {
-	for (c = 0; c < units[u].c; c++, i++) {
-            switch (units[u].s[0]) {
-            case 'S':
-	        for (p = 0; p < 6; p++)
-	            if (site_summary[u][c].retain[p]) {
-		        num_s_preds++;
-		        print_pred_info(fp, u, c, p);
-		        print_s_pred_name(fp, i, scalar_op[p / 2], p % 2 ? true : false);
-                        print_site_info(fp, i);
-	            }
-	        break;
-            case 'R':
-	        for (p = 0; p < 6; p++)
-	            if (site_summary[u][c].retain[p]) {
-		        num_r_preds++;
-	                print_pred_info(fp, u, c, p);
-		        print_r_pred_name(fp, i, return_op[p / 2], p % 2 ? true : false);
-                        print_site_info(fp, i);
-                    }
-	        break;
-            case 'B':
-	        for (p = 0; p < 2; p++)
-	            if (site_summary[u][c].retain[p]) {
-	                num_b_preds++;
-	                print_pred_info(fp, u, c, p);
-	                print_b_pred_name(fp, i, branch_op[p]);
-                        print_site_info(fp, i);
-                    }
-	        break;
             default:
                 assert(0);
             }
         }
     }
+}
 
-    fclose(fp);
+inline bool is_eligible(int u, int c, int p, int s)
+{
+    return site_summary[u][c].retain[p] && 
+           (isdigit(sites[s + c].args[1][0]) || sites[s + c].args[1][0] != '-'); 
+}
+
+void checkG(int u, int c, int p, int s)
+{
+    for (int d = 0; d < units[u].c; d++) {
+        if (c != d &&
+            sites[s + c].line == sites[s + d].line &&
+            strcmp(sites[s + c].args[0], sites[s + d].args[0]) == 0 &&
+            atoi(sites[s + c].args[1]) < atoi(sites[s + d].args[1]) &&
+            ((is_eligible(u, d, GT , s) && have_equal_increase(u, c, p, u, d, GT )) ||
+             (is_eligible(u, d, GEQ, s) && have_equal_increase(u, c, p, u, d, GEQ)))) {
+            site_summary[u][c].retain[p] = false;
+            break;
+        }
+    }
+}
+
+void checkL(int u, int c, int p, int s)
+{
+    for (int d = 0; d < units[u].c; d++) {
+        if (c != d &&
+            sites[s + c].line == sites[s + d].line &&
+            strcmp(sites[s + c].args[0], sites[s + d].args[0]) == 0 &&
+            atoi(sites[s + c].args[1]) > atoi(sites[s + d].args[1]) &&
+            ((is_eligible(u, d, LT , s) && have_equal_increase(u, c, p, u, d, LT )) ||
+             (is_eligible(u, d, LEQ, s) && have_equal_increase(u, c, p, u, d, LEQ)))) {
+            site_summary[u][c].retain[p] = false;
+            break;
+        }
+    }
+}
+
+void cull_preds_aggressively2()
+{
+    int u, c, s;
+
+    for (s = 0, u = 0; u < NumUnits; s += units[u].c, u++) {
+        if (units[u].s[0] == 'S') {
+	    for (c = 0; c < units[u].c; c++) {
+                if (is_eligible(u, c, GT , s))
+                    checkG(u, c, GT , s);
+                if (is_eligible(u, c, GEQ, s))
+                    checkG(u, c, GEQ, s);
+                if (is_eligible(u, c, LT , s))
+                    checkL(u, c, LT , s);
+                if (is_eligible(u, c, LEQ, s))
+                    checkL(u, c, LEQ, s);
+            }
+        }
+    }
 }
 
 /****************************************************************************
@@ -487,7 +549,8 @@ int main(int argc, char** argv)
     scaffold(compact_report_path_fmt, process_site);
 
     cull_preds();
-    cull_preds_aggressively();
+    cull_preds_aggressively1();
+    cull_preds_aggressively2();
 
     print_retained_preds();
     print_result_summary();
