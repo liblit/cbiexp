a358425d33ea99ba8f99e23779d352cbff8e591d
Ben Liblit <liblit@cs.wisc.edu>
2004-08-03 13:34:22 +0000

    Fiddle around with variable scopes to keep them as narrow as possible without needlessly rerunning constructors. Demote some friendly runtime checks into assertions.  The file we're reading here should already be in a trusted, known-good form.

--- src/ReportReader.cc
+++ src/ReportReader.cc
@@ -1,3 +1,4 @@
+#include <cassert>
 #include <cstdio>
 #include <iostream>
 #include "CompactReport.h"
@@ -14,56 +15,41 @@ ReportReader::read(unsigned runId) const
 {
   FILE * const report = fopenRead(CompactReport::format(runId));
 
-  unsigned x, y, z, w;
-  bool problem = false;
+  SiteCoords coords;
 
   while (true)
     {
-      SiteCoords coords;
       fscanf(report, "%u\t%u\t", &coords.unitIndex, &coords.siteOffset);
       if (feof(report))
 	break;
-      if (coords.unitIndex >= num_units)
+
+      assert(coords.unitIndex < num_units);
+      const unit_t unit = units[coords.unitIndex];
+      assert(coords.siteOffset < unit.num_sites);
+      switch (unit.scheme_code)
 	{
-	  cerr << "unit " << coords.unitIndex << " exceeds unit count (" << num_units << ")\n";
-	  problem = true;
-	}
-      else
-	{
-	  const unit_t unit = units[coords.unitIndex];
-	  if (coords.siteOffset >= unit.num_sites)
-	    {
-	      cerr << "site " << coords.siteOffset << " exceeds unit's site count (" << unit.num_sites << ")\n";
-	      problem = true;
-	    }
-	  else
-	    switch (unit.scheme_code)
-	      {
-	      case 'B':
-		fscanf(report, "%u\t%u\n", &x, &y);
-		branchesSite(coords, x, y);
-		break;
-	      case 'G':
-		fscanf(report, "%u\t%u\t%u\t%u\n", &x, &y, &z, &w);
-		gObjectUnrefSite(coords, x, y, z, w);
-		break;
-	      case 'R':
-		fscanf(report, "%u\t%u\t%u\n", &x, &y, &z);
-		returnsSite(coords, x, y, z);
-		break;
-	      case 'S':
-		fscanf(report, "%u\t%u\t%u\n", &x, &y, &z);
-		scalarPairsSite(coords, x, y, z);
-		break;
-	      default:
-		cerr << "unit " << coords.unitIndex << " has unknown scheme code '" << unit.scheme_code << "'\n";
-		problem = true;
-	      }
+	  unsigned x, y, z, w;
+	case 'B':
+	  fscanf(report, "%u\t%u\n", &x, &y);
+	  branchesSite(coords, x, y);
+	  break;
+	case 'G':
+	  fscanf(report, "%u\t%u\t%u\t%u\n", &x, &y, &z, &w);
+	  gObjectUnrefSite(coords, x, y, z, w);
+	  break;
+	case 'R':
+	  fscanf(report, "%u\t%u\t%u\n", &x, &y, &z);
+	  returnsSite(coords, x, y, z);
+	  break;
+	case 'S':
+	  fscanf(report, "%u\t%u\t%u\n", &x, &y, &z);
+	  scalarPairsSite(coords, x, y, z);
+	  break;
+	default:
+	  cerr << "unit " << coords.unitIndex << " has unknown scheme code '" << unit.scheme_code << "'\n";
+	  exit(1);
 	}
     }
 
   fclose(report);
-
-  if (problem)
-    exit(1);
 }
