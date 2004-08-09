14860bf5f9ba3b2ea47e5f8053c7fd046366f4f6
Alice Zheng <alicez@gmail.com>
2004-08-09 01:34:32 +0000

    Modified calcCorr to use information from successful runs as well, i.e., using the matrices Ws, Wsobs, and Wscross.

--- correlations/calcCorr.m
+++ correlations/calcCorr.m
@@ -1,19 +1,19 @@
 Wf = full(Wf);
-%Ws = full(Ws);
+Ws = full(Ws);
 Wfobs = full(Wfobs);
-%Wsobs = full(Wsobs);
+Wsobs = full(Wsobs);
 % Wfcross(i,j) is number of times i is observed and j is true
 Wfcross = full(Wfcross);  
-%Wscross = full(Wscross);
+Wscross = full(Wscross);
 N = size(Wf,2);
 
 %% P(A true | A obs, B obs)
-%PacondB = (Wfcross + Wscross)'./(Wfobs+Wsobs);
-PacondB = Wfcross'./Wfobs;
+PacondB = (Wfcross + Wscross)'./(Wfobs+Wsobs);
+%PacondB = Wfcross'./Wfobs;
 PacondB(isnan(PacondB)) = 0;
 %% P(A and B true | A obs, B obs)
-%Pab = (Wf+Ws)./(Wfobs+Wsobs);
-Pab = Wf./Wfobs;
+Pab = (Wf+Ws)./(Wfobs+Wsobs);
+%Pab = Wf./Wfobs;
 Pab(isnan(Pab)) = 0;
 Varcond = PacondB .* (1- PacondB);
 Cov = Pab - PacondB.*PacondB'; 
