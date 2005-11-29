5a28dcbfb81f3161c967dee25c069062ab2d63b5
Ben Liblit <liblit@cs.wisc.edu>
2005-11-29 01:40:28 +0000

    Oops, add a simple but important header omitted from the last commit.

---
+++ src/corrective-ranking/Foci.h
@@ -0,0 +1,10 @@
+#ifndef INCLUDE_corrective_ranking_Foci_h
+#define INCLUDE_corrective_ranking_Foci_h
+
+#include <ext/hash_set>
+
+
+typedef __gnu_cxx::hash_set<unsigned> Foci;
+
+
+#endif // !INCLUDE_corrective_ranking_Foci_h
