5dbd20ea9a2e05b54ac2c8bbaee4657cd3aa47ab
Anne Mulhern <mulhern@cs.wisc.edu>
2006-12-13 03:00:55 +0000

    Using sparse matrix functionality since we're using matlab and it is much faster.

--- src/lsa/convertData.m
+++ src/lsa/convertData.m
@@ -1,10 +1,8 @@
 function convertData()
     Findices = load('f.indices');
     Sindices = load('s.indices');
-    Z = load('X.sparse');
-    for i = 1:size(Z,1);
-        X(Z(i,1),Z(i,2)) = Z(i,3);
-    end;
+    X = load('X.sparse');
+    X = spconvert(X);
     save -mat runsinfo.mat Findices Sindices X
     quit; 
 
