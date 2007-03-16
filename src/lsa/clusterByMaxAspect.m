0507f45852586208f71cab5bd026bf4b6429ca36
Anne Mulhern <mulhern@cs.wisc.edu>
2007-03-16 15:11:41 +0000

    Matlab changes the meaning of some commands when it encounters a vector instead of a matrix. This is a problem if you want it to act consistently, so one needs to add extra arguments to the command, e.g., max, to force it to interpret the vector as a matrix.

--- src/lsa/clusterByMaxAspect.m
+++ src/lsa/clusterByMaxAspect.m
@@ -15,27 +15,33 @@ function clusterByMaxAspect()
 
     % for failures, find buggy aspect with maximum probability 
     Rows = numusageaspects + 1:numaspects;
-    BugClusters = findBestAspect(Pz_d,Rows, Findices);
-    BugClusters = padColumns(BugClusters, numruns);
+    BugClusters = findBestIndex(Pz_d,Rows, Findices);
 
     % for all runs, find usage aspects with maximum probability
-    UsageClusters = findBestAspect(Pz_d, [1:numusageaspects], [1:numruns]);
+    UsageClusters = findBestIndex(Pz_d, [1:numusageaspects], [1:numruns]);
 
     BugByUsage = spones(BugClusters) * spones(UsageClusters)'; 
 
     %for all runs, find aspect with maximum probability
-    Clusters = findBestAspect(Pz_d, [1:numaspects], [1:numruns]);
+    Clusters = findBestIndex(Pz_d, [1:numaspects], [1:numruns]);
 
     save clusters.mat BugClusters UsageClusters BugByUsage Clusters
     quit
 
-function Res = findBestAspect(X,Rows,Cols)
-    P = X(Rows,:);  
-    [M,I] = max(P(:,Cols));
+% Res - m * n matrix, if the ith value is the maximum for the jth column, 
+% Res(i,j) = X(i,j), else, Res(i,j) = 0.
+% X - the original m * n matrix 
+% Rows - a row selection vector
+% Cols - a column selection vector
+function Res = findBestIndex(X,Rows,Cols)
+    P = X(Rows,Cols);  
+    [M,I] = max(P,[],1);
     for i = 1:size(P,1);
         v = find(I == i); 
         Res(i,Cols(v)) = M(v);
     end;
+    Res = padColumns(Res, size(X,2));  
+    return;
 
 function O = padColumns(O,n)
     if size(O,2) < n;
