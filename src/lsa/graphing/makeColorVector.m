8fc774915f0fb974e228f9648bb89fc7b9597be6
Anne Mulhern <mulhern@cs.wisc.edu>
2007-03-25 16:37:55 +0000

    Little utilities for generating a color vector based to use in a scatter plot for marking data points.

---
+++ src/lsa/graphing/makeColorVector.m
@@ -0,0 +1,11 @@
+% V -- the color row vector with indices, indices are distributed on a color map
+% M -- matrix with rows corresponding to the property we want to color 
+function V = makeColorVector(M)
+    if not(isempty(find(sum(M,1) > 1)));
+        error('M matrix is incorrectly formatted');
+    end;
+
+    [I,J] = ind2sub(size(M), find(M));
+    V(1,size(M,2)) = 0;
+    V(1,J) = I;
+return;
