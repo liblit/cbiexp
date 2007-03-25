7d8865b3ca249559794f14f176eda757bf0b4027
Anne Mulhern <mulhern@cs.wisc.edu>
2007-03-25 03:16:37 +0000

    Throwing out all runs that have 0 counts for all features.

--- src/lsa/runOnce.m
+++ src/lsa/runOnce.m
@@ -15,6 +15,10 @@ function runOnce()
     seed = sum(100*clock);
     rand('state',seed);
 
+    [X,I] = removeZeroColumns(X);
+
+    Sindices = adjustIndices(Sindices, I);
+
     [Pw_z,Pd_z,Pz,Li] = pLSA_EM(X, Learn, Sindices);
     
     Pw = marginalize(Pw_z, Pz);
@@ -24,7 +28,7 @@ function runOnce()
     Pd_w = Pd_z * Pz_w;
     Pw_d = Pw_z * Pz_d; 
 
-    save -mat results.mat Pw_z Pd_z Pz Pw Pd Pz_w Pz_d Pd_w Pw_d Li seed Learn
+    save -mat results.mat Pw_z Pd_z Pz Pw Pd Pz_w Pz_d Pd_w Pw_d Li seed Learn I
 
     out = fopen('loglikelihood.txt', 'w');
     fprintf(out, '%.0f\n', Li(end));
