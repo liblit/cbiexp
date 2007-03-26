25a49f1d9836fbd9bc830319e28490c40b7e9142
Anne Mulhern <mulhern@cs.wisc.edu>
2007-03-26 01:44:38 +0000

    For the flags matrix, the number of rows is dependent on the number of flags--- the number of columns on the number of runs.

--- src/lsa/convertFlagsInfo.m
+++ src/lsa/convertFlagsInfo.m
@@ -1,13 +1,14 @@
 function convertFlagsInfo()
     F = load('flags.sparse'); 
-    dimensions = load('X.dimensions');
-    Flags = convertFlags(F, dimensions);
+    Xdimensions = load('X.dimensions');
+    Fdimensions = load('flags.dimensions');
+    Flags = convertFlags(F, [Fdimensions(1) Xdimensions(2)]);
     save -mat flagsinfo.mat Flags
     quit; 
 
 % Pads the matrix to the appropriate length
 function X = convertFlags(X,dimensions)
     X = spconvert(X);
-    if not(dimensions(2) == size(X,2));
-        X(size(X,1),dimension(2)) = 0;
+    if not(all(dimensions == size(X)));
+        X(dimensions(1),dimensionis(2)) = 0;
     end;
