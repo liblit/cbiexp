3ab7fde8ff10c2acc47ec022936659fdde0927d7
Anne Mulhern <mulhern@cs.wisc.edu>
2007-03-27 19:40:22 +0000

    Was not actually correct algorithm. Greedy algorithm worked on only a subset of possible scores. Corrected algorithm selects top scores from all possible scores.

--- src/lsa/oracle/compareClustersToOracle.m
+++ src/lsa/oracle/compareClustersToOracle.m
@@ -7,28 +7,25 @@ function compareClustersToOracle()
     numbugaspects = Learn.Kb;
 
     C = spones(BugClusters(:,Findices));
-    c = size(C,1);
     B = Bugs(:,Findices); 
-    b = size(B,1);
 
-    % pad matrices as necessary
-    if b > c;
-        C(c + 1 : b, :) = 0; 
-    elseif c > b;
-        B(b + 1 : c, :) = 0; 
+    Right = C * B';
+    Wrong = C * not(B');
+    Scores = Right - Wrong;
+    ClusterMat = zeros(size(Scores));
+    Is = ones(size(ClusterMat, 1), 1); 
+    Js = ones(1, size(ClusterMat, 2));   
+    while (any(Is) & any(Js));
+        Iinds = find(Is);
+        Jinds = find(Js);
+        [M,I] = max(Scores(Iinds, Jinds), [], 1);
+        [m,j] = max(M);
+        ClusterMat(Iinds(I(j)), Jinds(j)) = 1;
+        Is(Iinds(I(j)),1) = 0;
+        Js(1, Jinds(j)) = 0;
     end;
 
-    for i = 1:numbugaspects;
-        Right = C(i,:) * B';
-        Wrong = C(i,:) * not(B');
-        Scores = Right - Wrong; 
-        [m,index] = max(Scores);
-        ClusterMap(i,1:3) = [index Right(index) Wrong(index)];  
-    end;
-
-    OracleByUsage = Bugs * spones(UsageClusters)';
-
-    save clustermap.mat ClusterMap OracleByUsage;
+    save clustermap.mat ClusterMat Scores Right Wrong
     quit
         
 
