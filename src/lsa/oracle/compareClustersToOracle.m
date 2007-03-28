8ad90cf298817c1f6258a87867a2d9c6f7962126
Anne Mulhern <mulhern@cs.wisc.edu>
2007-03-28 02:25:55 +0000

    Generate confusion matrix.

--- src/lsa/oracle/compareClustersToOracle.m
+++ src/lsa/oracle/compareClustersToOracle.m
@@ -12,20 +12,28 @@ function compareClustersToOracle()
     Right = C * B';
     Wrong = C * not(B');
     Scores = Right - Wrong;
-    ClusterMat = zeros(size(Scores));
-    Is = ones(size(ClusterMat, 1), 1); 
-    Js = ones(1, size(ClusterMat, 2));   
-    while (any(Is) & any(Js));
+    ClusterMat = makeMap(Scores);
+    Confusion = makeConfusion(ClusterMat, C, B); 
+    save clustermap.mat ClusterMat Scores Right Wrong Confusion
+    quit
+
+function Map = makeMap(Scores)
+    Map = zeros(size(Scores));
+    Js = ones(1, size(Map, 2));   
+    Is = ones(size(Map,1), 1);
+    while (any(Js) & any(Is));
         Iinds = find(Is);
         Jinds = find(Js);
         [M,I] = max(Scores(Iinds, Jinds), [], 1);
         [m,j] = max(M);
-        ClusterMat(Iinds(I(j)), Jinds(j)) = 1;
-        Is(Iinds(I(j)),1) = 0;
+        Map(Iinds(I(j)), Jinds(j)) = 1;
+        Is(Iinds(I(j)), 1) = 0;
         Js(1, Jinds(j)) = 0;
     end;
 
-    save clustermap.mat ClusterMat Scores Right Wrong
-    quit
-        
-
+function Confusion = makeConfusion(ClusterMat, C, B)
+    for i = 1:size(ClusterMat,1);
+        v = find(ClusterMat(i,:)); 
+        C1(v,:) = repmat(C(i,:), numel(v), 1); 
+    end;
+    Confusion = (C1 * B')';
