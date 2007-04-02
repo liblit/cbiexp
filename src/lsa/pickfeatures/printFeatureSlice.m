7c2f3fdff6913366e8fe655e936e0103c33dce93
Anne Mulhern <mulhern@cs.wisc.edu>
2007-04-02 23:55:33 +0000

    Scripts for assisting in grabbing features once visually identified.

---
+++ src/lsa/pickfeatures/printFeatureSlice.m
@@ -0,0 +1,8 @@
+function printFeatureSlice(filename, I)
+   doc = com.mathworks.xml.XMLUtils.createDocument('features');
+   docRoot = doc.getDocumentElement();
+   xmlify(doc, docRoot, {num2cell(I)}, 'feature', {'index'});
+   xmlwrite(filename, doc);
+   quit();
+   
+     
