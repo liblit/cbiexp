547003b24bbb9e36fd76e1c09d14f5f61cd6c327
Anne Mulhern <mulhern@cs.wisc.edu>
2007-04-24 21:18:03 +0000

    Factored out some shared code. Converter for ICML data also shares this code.

--- src/lsa/convertBugsInfo.m
+++ src/lsa/convertBugsInfo.m
@@ -2,13 +2,5 @@ function convertBugsInfo(outfilename)
     Bugs = load('bugs.sparse'); 
     Xdimensions = load('X.dimensions');
     Indexing = indexing();
-    Bugs = convertBugs(Bugs, Xdimensions(2), Indexing);
+    Bugs = convertInfo(Bugs, Xdimensions(2), Indexing);
     save('-mat', outfilename, 'Bugs')
-
-% Pads the matrix to the appropriate length
-function X = convertBugs(X, numruns, Indexing)
-    X = spconvert(X);
-    X = X(:,Indexing.offset + 1 : end);
-    if not(all(numruns == size(X,2)));
-        X(end, numruns) = 0;
-    end;
--- src/lsa/convertFlagsInfo.m
+++ src/lsa/convertFlagsInfo.m
@@ -2,13 +2,5 @@ function convertFlagsInfo(outfilename)
     F = load('flags.sparse'); 
     Xdimensions = load('X.dimensions');
     Indexing = indexing();
-    Flags = convertFlags(F, Xdimensions(2), Indexing); 
+    Flags = convertInfo(F, Xdimensions(2), Indexing); 
     save('-mat', outfilename, 'Flags')
-
-% Pads the matrix to the appropriate length
-function X = convertFlags(X, numruns, Indexing)
-    X = spconvert(X);
-    X = X(:, Indexing.offset + 1 : end);
-    if not(all(numruns == size(X,2)));
-        X(end, numruns) = 0;
-    end;
