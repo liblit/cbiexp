f024aabde324a5adef202ee94e216aadbd0f74e6
Anne Mulhern <mulhern@cs.wisc.edu>
2007-08-10 02:53:40 +0000

    Handle multiple bugs for one run in approved way.

--- src/evaluateplsa/calculateRandIndex.m
+++ src/evaluateplsa/calculateRandIndex.m
@@ -1,37 +1,37 @@
-function = calculateRandIndex(bugfile, resultsfile, datafile, outputfile)
+function calculateRandIndex(bugfile, resultsfile, datafile, outputfile)
     load(bugfile);
     load(resultsfile); 
     load(datafile);
     Cluster = clusterByMaxValue(Results.Pz_d(Results.Learn.BuggyIndices,:));
     Bugs = Bugs(:,Data.Indices);
 
-    Cluster = Cluster(:,find(Data.Findices));
-    Bugs = Bugs(:,find(Data.Findices));
+    Findices = find(Data.Findices); 
+    Cluster = Cluster(:, Findices);
+    Bugs = Bugs(:, Findices);
 
     if any(sum(Bugs,1) < 1);
         error('Failing run must have at least one bug cause.')
     end;
 
-    ms = find(sum(Bugs,1) == 1);
+    numfailures = numel(Findices); 
 
-    Cluster = Cluster(:,ms);
-    Bugs = Bugs(:,ms);
+    % each run gets a unique id; the indices of the bug causes in the array
+    % determine a binary number
+    Bugs = (Bugs' * (2.^((1:size(Bugs,1)) - 1))')';
 
     num = 0;
-    antinum = 0;
-    for i = 1:size(Bugs,2);
-        b = find(Bugs(:,i));
+    for i = 1:numfailures;
+        b = Bugs(i); 
         c = Cluster(i);
-        for j = i + 1:size(Bugs,2);
-            b_other = find(Bugs(:,j));
+        for j = (i + 1):numfailures;
+            b_other = Bugs(j); 
             c_other = Cluster(j);
             agreement = or(and(b == b_other, c == c_other), and(b ~= b_other, c ~= c_other));
             num = num + agreement;
-            antinum = antinum + ~agreement;
         end
     end
 
-    Rand = num/double(nchoosek(numel(ms), 2));
+    Rand = num/double(nchoosek(numfailures, 2));
 
     out = fopen(outputfile, 'w');
     fprintf(out, '%.4f\n', Rand);
