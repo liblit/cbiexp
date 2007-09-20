3a17b0eba6b8d83b66e4db9068162f5420bc4c7b
Anne Mulhern <mulhern@cs.wisc.edu>
2007-09-20 14:35:14 +0000

    Changes that reflect the fact that sometimes we only want to generate data for buggy topics---in some contexts generating data for all topics makes no sense.

--- src/fromplsa/makeBaseFiles.m
+++ src/fromplsa/makeBaseFiles.m
@@ -1,7 +1,17 @@
-function makeBaseFiles(infilename)
+function makeBaseFiles(infilename, which)
     load(infilename)
-    for i = 1:Results.Learn.numaspects; 
-        fid = fopen(['aspect_' num2str(i) '.base'], 'w');
-        fprintf(fid, '%u', i);
-        fclose(fid);
-    end;
+    if (which == 'all');
+        for i = 1:Results.Learn.numaspects; 
+            fid = fopen(['aspect_' num2str(i) '.base'], 'w');
+            fprintf(fid, '%u', i);
+            fclose(fid);
+        end;
+    elseif (which == 'buggy'); 
+        for i = Results.Learn.BuggyIndices; 
+            fid = fopen(['aspect_' num2str(i) '.base'], 'w');
+            fprintf(fid, '%u', i);
+            fclose(fid);
+        end
+    else
+        error('Specified base file indices are not recognized.')
+    end
