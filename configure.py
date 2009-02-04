05a355bb435387c8f770c3762e130b83f7bf6772
Anne Mulhern <mulhern@cs.wisc.edu>
2009-02-04 15:09:19 +0000

    Whoops, fix a spelling error.

--- configure.py
+++ configure.py
@@ -3,7 +3,7 @@
 from sys import argv, exit
 from subprocess import call
 
-command = ['./configure', 'GXX=/s/gcc-4.3.2/bin/g++']
+command = ['./configure', 'CXX=/s/gcc-4.3.2/bin/g++']
 command += argv[1:]
 
 exit(call(command))
