93b858c3c312a375565ddde0aaabafea504bcfe4
Suhail Shergill <shergill@cs.wisc.edu>
2009-11-24 15:13:15 +0000

    undo-ing faulty commit

--- src/sitesIntoDB.py
+++ src/sitesIntoDB.py
@@ -7,10 +7,74 @@ import re
 import sqlite3
 import sys
 
-from itertools import imap, count
-from initializeSchema import EnumerationTables
+from itertools import imap
 
-# Assuming all tables have already been created by initializeSchema.py
+###############################################################################
+# Table creation queries
+###############################################################################
+
+CreateQueries = """
+CREATE TABLE AccessTypes(AccessType TEXT PRIMARY KEY NOT NULL);
+INSERT INTO AccessTypes VALUES('read');
+INSERT INTO AccessTypes VALUES('write');
+
+CREATE TABLE AssignmentTypes(AssignmentType TEXT PRIMARY KEY NOT NULL);
+INSERT INTO AssignmentTypes VALUES('direct');
+INSERT INTO AssignmentTypes VALUES('field');
+INSERT INTO AssignmentTypes VALUES('index');
+
+CREATE TABLE VariableTypes(VariableType TEXT PRIMARY KEY NOT NULL);
+INSERT INTO VariableTypes VALUES('local');
+INSERT INTO VariableTypes VALUES('global');
+INSERT INTO VariableTypes VALUES('mem');
+
+CREATE TABLE Sites(
+    ID INTEGER PRIMARY KEY NOT NULL,
+    FileName TEXT NOT NULL,
+    Line INT NOT NULL,
+    FunctionIdentifier TEXT NOT NULL,
+    CFGNode INT NOT NULL,
+    Scheme_ID TEXT NOT NULL);
+
+CREATE TABLE AtomsSiteDescriptors(
+    ID INTEGER PRIMARY KEY REFERENCES Sites_pk,
+    Variable TEXT NOT NULL,
+    AccessType TEXT REFERENCES AccessTypes_pk);
+
+CREATE TABLE BoundsSiteDescriptors(
+    ID INTEGER PRIMARY KEY REFERENCES Sites_pk,
+    Variable TEXT NOT NULL,
+    VariableType TEXT REFERENCES VariableTypes_pk,
+    AccessType TEXT REFERENCES AccessTypes_pk);
+
+CREATE TABLE BranchesSiteDescriptors(
+    ID INTEGER PRIMARY KEY REFERENCES Sites_pk,
+    Predicate TEXT NOT NULL);
+
+CREATE TABLE FloatKindsSiteDescriptors(
+    ID INTEGER PRIMARY KEY REFERENCES Sites_pk,
+    Variable TEXT NOT NULL,
+    VariableType TEXT REFERENCES VariableTypes_pk,
+    AccessType TEXT REFERENCES AccessTypes_pk);
+
+CREATE TABLE GObjectUnrefSiteDescriptors(
+    ID INTEGER PRIMARY KEY REFERENCES Sites(ID),
+    Object TEXT NOT NULL);
+
+CREATE TABLE ReturnsSiteDescriptors(
+    ID INTEGER PRIMARY KEY REFERENCES Sites(ID),
+    Callee TEXT NOT NULL);
+
+CREATE TABLE ScalarPairsSiteDescriptors(
+    ID INTEGER PRIMARY KEY REFERENCES Sites(ID),
+    LHSVariable TEXT NOT NULL,
+    LHSVariableType TEXT REFERENCES VariableTypes_pk,
+    LHSAssignmentType TEXT REFERENCES AssignmentTypes_pk,
+    RHSVariable TEXT NOT NULL,
+    RHSVariableType TEXT REFERENCES VariableTypes_pk);
+"""
+
+
 ###############################################################################
 
 schemeTables = {
@@ -38,8 +102,8 @@ numTableColumns = {
 ###############################################################################
 
 def readSites(filepath):
-    keys = ('SiteID',
-            'SchemeID',
+    keys = ('ID',
+            'Scheme_ID',
             'FileName',
             'Line',
             'FunctionIdentifier',
@@ -47,20 +111,17 @@ def readSites(filepath):
             'Descriptors')
     matcher = re.compile('<sites unit="(?P<sig>[0-9a-f]{32})" scheme="(?P<sch>[a-zA-Z\-]*)">')
 
-    SiteID = count()
-    SchemeID = None
+    Scheme_ID = None
     with file(filepath, 'r') as ifile:
-        for lineNum, line in enumerate(ifile):
+        for ID, line in enumerate(ifile):
             match = matcher.match(line)
             if match:
-                Scheme = match.group('sch')
-                SchemeID = filter(lambda x: x[1]==Scheme, 
-                                    EnumerationTables['Schemes'])[0][0]
+                Scheme_ID = match.group('sch')
             elif line.startswith('</sites>'):
                 # closing sites tag
-                SchemeID = None
+                Scheme_ID = None
             else:
-                vals = [SiteID.next(), SchemeID] + line.strip().split(None, 4)
+                vals = [ID, Scheme_ID] + line.strip().split(None, 4)
                 yield dict(zip(keys, vals))
 
 
@@ -68,21 +129,22 @@ def process(sitesFile, dbname):
     con = sqlite3.connect(dbname)
     cursor = con.cursor()
 
+    cursor.executescript(CreateQueries)
+
     for site in readSites(sitesFile):
         cursor.execute("INSERT INTO Sites VALUES(\
-                            :SiteID, :FileName, :Line, :FunctionIdentifier,\
-                            :CFGNode, :SchemeID);",
+                            :ID, :FileName, :Line, :FunctionIdentifier,\
+                            :CFGNode, :Scheme_ID);",
                         site)
 
-        scheme = filter(lambda x: x[0] == site['SchemeID'],
-                            EnumerationTables['Schemes'])[0][1]
+        scheme = site['Scheme_ID']
         if schemeTables[scheme]:
             table = schemeTables[scheme]
             numCols = numTableColumns[scheme]
             entries = ', '.join('?' * numCols)
 
             query = "INSERT INTO %s VALUES (%s);" % (table, entries)
-            cursor.execute(query, [site['SiteID']] + site['Descriptors'].split('\t'))
+            cursor.execute(query, [site['ID']] + site['Descriptors'].split('\t'))
 
     con.commit()
 
