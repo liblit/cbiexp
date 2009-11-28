2b27c9f0b48ba67932a4fd3d0f386684ac2ae6fc
Piramanayagam Arumuga Nainar <piramanayagam@gmail.com>
2009-11-28 23:02:18 +0000

    Update strings that generate static-site-info.cc, s.runs, f.runs to use the new database format rather than individual files like sites.txt and outcomes.txt. - analysis-rules.mk.in expects the $(database) variable to be set. - Field names in the Cheetah template file now match column names in the database.

--- src/genOutcomeIndices.py
+++ src/genOutcomeIndices.py
@@ -1,32 +1,53 @@
-#!/usr/bin/python
-from itertools import count, izip
+#!/s/python-2.5/bin/python
+from __future__ import with_statement
+
+import sqlite3
+
 from optparse import OptionParser
+from os.path import exists
+
+def queryOutcomeEnum(conn):
+    """retrieve enumerations from the 'Outcomes' table into a dict"""
+
+    cursor = conn.cursor()
+    cursor.execute('SELECT Name, OutcomeID from Outcomes')
+    OutcomeIDs = dict(iter(cursor))
+    cursor.close()
+
+    return OutcomeIDs
+
+def writeRunIDs(conn, outcome, output):
+    """write RunIDs with a particular <outcome> to <output>"""
+
+    query = 'SELECT RunID from Runs WHERE OutcomeID=? ORDER BY RunID'
+    cursor = conn.cursor()
+    cursor.execute(query, (outcome,))
+
+    with file(output, 'w') as ofile:
+        for row in cursor:
+            print >>ofile, row[0]
+
+    cursor.close()
+
+def genOutcomeIndices(cbi_db, sFile, fFile):
+    """read outcomes from database and generate f.runs + s.runs"""
+
+    if not exists(cbi_db):
+        parser.error('sqlite database %s does not exist' % cbi_db)
+
+    conn = sqlite3.connect(cbi_db)
+
+    OutcomeIDs = queryOutcomeEnum(conn)
+    writeRunIDs(conn, OutcomeIDs['SUCCESS'], sFile)
+    writeRunIDs(conn, OutcomeIDs['FAILURE'], fFile)
+    conn.close()
 
 def main():
-    """read outcomes and generate f.runs + s.runs"""
-    parser = OptionParser(usage='%prog outcomesfile ffile sfile')
-
+    parser = OptionParser(usage='%prog <database> <s.runs-file> <f.runs-file>')
     options, args = parser.parse_args()
     if len(args) != 3: parser.error('wrong number of positional arguments')
 
-    ffile = open(args[1], 'w')
-    sfile = open(args[2], 'w')
-
-    def thefile(outcome):
-        if outcome == 0:
-            return sfile
-        if outcome > 0:
-            return ffile
-        raise ValueError('unexpected value for outcome %s' % outcome)
-
-    ofile = open(args[0], 'r')
-    for (c, t) in izip(count(0), ofile):
-        print >>thefile(int(t.strip())), c
-
-    ofile.close()
-
-    ffile.close()
-    sfile.close()
+    genOutcomeIndices(*args)
 
 if __name__ == "__main__":
     main()
