e0032e28a3fcfdd23f5402af3bf86ee3de49c230
Piramanayagam Arumuga Nainar <piramanayagam@gmail.com>
2009-12-04 23:25:49 +0000

    Update ReportReader to read from a database instead of the counts.txt file.  Test of one ReportReader user (prefilter) for one experiment (tcas.c, v1) yields correct results.

--- src/ReportReader.h
+++ src/ReportReader.h
@@ -2,8 +2,7 @@
 #define INCLUDE_ReportReader_h
 
 #include "Counts.h"
-#include <iostream>
-#include <fstream>
+#include <sqlite3.h>
 #include <vector>
 
 using namespace std;
@@ -14,15 +13,17 @@ class ReportReader
 {
 public:
   ReportReader(const char* filename);
-  vector <streampos> positions; //vector of positions for each run
-  string countfile; //the file from which reports are read
+  string dbfile; //the sqlite database
+  sqlite3 *db;
+
   virtual ~ReportReader() { }
   void read(unsigned runId);
-
 
 protected:
   virtual void handleSite(const SiteCoords &, std::vector<count_tp> &) = 0;
 
+private:
+  void throwError(const char *msg, const char *file, int line);
 };
 
 
