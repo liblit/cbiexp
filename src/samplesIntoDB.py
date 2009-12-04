0cc6b4712ef6e79d127c6229c2c96167dba217f2
Piramanayagam Arumuga Nainar <piramanayagam@gmail.com>
2009-12-04 23:20:37 +0000

    - write runs and outcomes into the Runs table. - make required file-name arguments as a part of positional arguments instead of optional flags.

--- src/samplesIntoDB.py
+++ src/samplesIntoDB.py
@@ -9,10 +9,39 @@ import optparse
 import os
 
 from collections import defaultdict
-from itertools import count
+from itertools import count, izip, imap
 
 from initializeSchema import EnumerationTables
 
+###############################################################################
+# Insert runs and outcomes from outcomes.txt
+###############################################################################
+
+def writeRunsIntoDB(conn, outcomesTxt):
+    """ Populate Runs table from file 'outcomesTxt'.  The file
+        contains one integer per line, representing the outcome
+        of runs, starting at index 0.
+    """
+
+    def getOutcome(line):
+        outcome = int(line.strip())
+        if outcome == 0:
+            return 0
+        else:
+            return 1
+
+    runID = count()
+    values = izip(runID, imap(getOutcome, file(outcomesTxt)))
+
+    cursor = conn.cursor()
+    cursor.executemany('INSERT INTO Runs VALUES (?,?)', values)
+    cursor.close()
+    conn.commit()
+
+
+###############################################################################
+# Insert samples from counts.txt
+###############################################################################
 
 ValueRecordingSchemes = frozenset(('bounds',))
 
@@ -36,7 +65,7 @@ def getSchemeIDForSite(siteID, cursor, siteIDToSchemeCache = {}):
 
 
 
-def inputSamples(conn, countsTxt, phase, version):
+def writeSamplesIntoDB(conn, countsTxt, phase, version):
     """Input sample counts/values into the database.
 
         ARGUMENTS:
@@ -105,41 +134,48 @@ def inputSamples(conn, countsTxt, phase, version):
         for line in countsFile:
             inputSamplesForSingleRun(conn, runID.next(), line)
 
+    conn.execute('CREATE INDEX IndexByRunID ON SampleCounts(RunID)')
     conn.commit()
 
 
 def main(argv=None):
-    """Insert sampled counts/values into appropriate tables.
+    """ Insert run outcomes and sampled counts/values
+        into appropriate tables.
     """
 
     if argv is None:
         argv = sys.argv
 
-    parser = optparse.OptionParser(usage='%prog [options] <database>.')
-    parser.add_option('-f', '--counts-file', action='store', default=None,
-                        help = 'path to the counts.txt file')
+    parser = optparse.OptionParser(usage='%prog [options] <counts.txt-file> <outcomes.txt-file> <database>.')
     parser.add_option('-v', '--version', action='store', default=1,
                       help = 'version of schema to implement')
     parser.add_option('-p', '--phase', action='store', default=-1,
                         help = 'phase number associated with runs')
 
     options, args = parser.parse_args(argv[1:])
-    if len(args) != 1: parser.error('wrong number of positional arguments')
+    if len(args) != 3:
+        parser.error('wrong number of positional arguments')
 
-    cbi_db = args[0]
-    if os.path.exists(cbi_db):
-        if options.version != 1:
-            return ('CBI database schema version ' + options.version +
-                    ' is currently not supported')
-        else:
-            if os.path.exists(options.counts_file):
-                conn = sqlite3.connect(cbi_db)
-            else:
-                return 'Make sure the path to counts-file is correct'
-    else:
-        return 'Make sure the path to database file is correct'
+    countsTxt, outcomesTxt, cbi_db = args
 
-    inputSamples(conn, options.counts_file, options.phase, options.version)
+    def checkExists(kind, name):
+        if not os.path.exists(name):
+            parser.error('%s %s does not exist' % (kind, name))
+
+    checkExists('counts-file', countsTxt)
+    checkExists('outcomes-file', outcomesTxt)
+    checkExists('database', cbi_db)
+
+    if options.version != 1:
+        parser.error('CBI database schema version ' + options.version +
+                     ' is currently not supported')
+
+    conn = sqlite3.connect(cbi_db)
+
+    writeRunsIntoDB(conn, outcomesTxt)
+    writeSamplesIntoDB(conn, countsTxt, options.phase, options.version)
+
+    conn.close()
 
 
 if __name__ == '__main__':
